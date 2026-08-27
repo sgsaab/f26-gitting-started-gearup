0768d1e5aa94684eafd673fa289c59f415307fc6
sgsaab <sana_saab@brown.edu>
2026-08-27 13:15:16 -0400

    tests: remove and add TODO

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,29 +1,9 @@
 import { add2, sub2 } from './math';
 
-describe("add2", () => {
-  test("adds positive numbers", () => {
-    expect(add2(1, 1)).toEqual(2);
-  });
-
-  test("adds negative numbers", () => {
-    expect(add2(-23, -39)).toEqual(-62);
-  });
-
-  test("adds zero", () => {
-    expect(add2(42, 0)).toEqual(42);
+describe("setup", () => {
+  test("jest is configured correctly", () => {
+    expect(true).toBe(true);
   });
 });
 
-// describe("sub2", () => {
-//   test("subtracts positive numbers", () => {
-//     expect(sub2(1, 1)).toEqual(0);
-//   });
-
-//   test("subtracts negative numbers", () => {
-//     expect(sub2(-23, -39)).toEqual(16);
-//   });
-
-//   test("subtracts zero", () => {
-//     expect(sub2(42, 0)).toEqual(42);
-//   });
-// });
+// TODO: Have the agent write some tests for add2 and sub2
