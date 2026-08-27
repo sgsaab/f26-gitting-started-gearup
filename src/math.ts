fee32938462e10a66b70d8ebca31b32726539c68
sgsaab <sana_saab@brown.edu>
2026-08-27 12:13:29 -0400

    feat: subtracting

--- src/math.ts
+++ src/math.ts
@@ -2,14 +2,20 @@
 // node dist/sample.js
 
 export function add2(a: number, b: number): number {
+    return a+b;
+}
+
+export function sub2(a: number, b: number): number {
     // TODO: implement me with the 32 agent
     return 0;
 }
 
-let a = 11;
-let b = 21;
-let result = add2(a, b);
+let a = 0;
+let b = 32;
+let added = add2(a, b);
+let subtracted = sub2(a, b);
 
 console.log('a:', a);
 console.log('b:', b);
-console.log('a + b:', result);
+console.log('a + b:', added);
+console.log('a - b:', subtracted);
