e2effbc46c88c0973e90b2a355f5619148153e0f
sgsaab <sana_saab@brown.edu>
2026-08-27 12:03:07 -0400

    Initial Commit

---
+++ src/add.ts
@@ -0,0 +1,15 @@
+// Build this project with npm run build, then run it with 
+// node dist/sample.js
+
+export function add2(a: number, b: number): number {
+    // TODO: implement me with the 32 agent
+    return 0;
+}
+
+let a = 11;
+let b = 21;
+let result = add2(a, b);
+
+console.log('a:', a);
+console.log('b:', b);
+console.log('a + b:', result);
